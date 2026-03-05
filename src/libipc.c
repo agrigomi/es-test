9d3e8fb9030fe79c55e03e544ea6192f45330d0d
mike <mike@localhost.localdomain>
2026-03-05 21:39:04 +0200

    libipc: Interface update + implementation (WIP)
    Signed-off-by: mike <mike@localhost.localdomain>

--- src/libipc.c
+++ src/libipc.c
@@ -1,3 +1,6 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
 #include "libipc.h"
 
 #if USE_SOCKETS
@@ -10,10 +13,50 @@ int ipc_get_fd(_ipc_cxt_t *cxt) {
 }
 #endif
 
-int ipc_open(const char *ifc, int mode, _ipc_cxt_t *cxt) {
-	int r = E_FAIL;
+#if USE_SHARED_MEMORY
+_ipc_cxt_t *open_shared_memory(const char *ifc) {
+	_ipc_cxt_t *r = NULL;
 
 
+	return r;
+}
+#endif
+
+_ipc_cxt_t *ipc_server(const char *ifc, int mode) {
+	_ipc_cxt_t *r = NULL;
+
+	switch (mode) {
+		case IPC_MODE_SHM:
+			/* Open server side shared area for connect only */
+			r = open_shared_memory(ifc);
+			break;
+		case IPC_MODE_INET:
+			break;
+		case IPC_MODE_UNIX:
+			break;
+	}
+
+	return r;
+}
+
+_ipc_cxt_t *ipc_client(const char *dst, int mode) {
+	_ipc_cxt_t *r = NULL;
+	char ifc[MAX_SHM_NAME];
+
+	memset(ifc, 0, sizeof(ifc));
+
+	switch (mode) {
+		case IPC_MODE_SHM:
+			/* Create uniqie name for client shared memory */
+			snprintf(ifc, sizeof(ifc), "SMC%d", getpid());
+			/* Open client side shared area for data transfer */
+			r = open_shared_memory(ifc);
+			break;
+		case IPC_MODE_INET:
+			break;
+		case IPC_MODE_UNIX:
+			break;
+	}
 
 	return r;
 }
@@ -22,16 +65,16 @@ void ipc_close(_ipc_cxt_t *cxt) {
 
 }
 
-int ipc_listen(_ipc_cxt_t *cxt) {
-	int r = E_FAIL;
+_ipc_cxt_t *ipc_listen(_ipc_cxt_t *server_cxt) {
+	 _ipc_cxt_t *r = NULL;
 
 
 
 	return r;
 }
 
-int ipc_connect(_ipc_cxt_t *cxt, const char *dst) {
-	int r = E_FAIL;
+int ipc_connect(_ipc_cxt_t *cxt) {
+	int r = E_IPC_FAIL;
 
 
 
--- src/libipc.h
+++ src/libipc.h
@@ -10,22 +10,22 @@
 #endif
 
 
-#define IPC_MODE_SHM		1
-#define IPC_MODE_INET		2
-#define IPC_MODE_UNIX		3
+#define IPC_MODE_SHM		1	/* Shared memory mode */
+#define IPC_MODE_INET		2	/* Internet sockets */
+#define IPC_MODE_UNIX		3	/* Local sockets */
 
 /* Error defs */
-#define E_OK		0
-#define E_FAIL		-1
+#define E_IPC_OK		0
+#define E_IPC_FAIL		-1
 
 /* IPC context */
 typedef struct {
-	int	mode;	/* IPC mode */
+	int		mode;		/* IPC mode */
 #if USE_SHARED_MEMORY
 	sem_t		s_data;		/* request semaphore */
 	sem_t		s_result;	/* result semaphore */
 	char		shm_name[MAX_SHM_NAME]; /* shared memory name */
-	unsigned int	size; /* data size */
+	unsigned int	size; 		/* data size */
 	unsigned char	io_buffer[MAX_IO_BUFFER];
 #endif
 } _ipc_cxt_t;
@@ -33,15 +33,28 @@ typedef struct {
 #ifdef __cplusplus
 extern "C" {
 #endif
+
 #if USE_SOCKETS
 int ipc_get_fd(_ipc_cxt_t *cxt);
 #endif
 
-int ipc_open(const char *ifc, int mode, _ipc_cxt_t *cxt);
-void ipc_close(_ipc_cxt_t *cxt);
-int ipc_listen(_ipc_cxt_t *cxt);
-int ipc_connect(_ipc_cxt_t *cxt, const char *dst);
+/* Server side.
+ * Create IPC context */
 
+_ipc_cxt_t *ipc_server(const char *ifc, int mode);
+/* Server side listen for client connections.
+ * Returns connection IPC context */
+_ipc_cxt_t *ipc_listen(_ipc_cxt_t *server_cxt);
+
+/* Client side.
+ * Returns connection IPC context */
+_ipc_cxt_t *ipc_client(const char *dst, int mode);
+
+/* Client side.
+   Connect to server. */
+int ipc_connect(_ipc_cxt_t *cxt);
+
+void ipc_close(_ipc_cxt_t *cxt);
 #ifdef __cplusplus
 }
 #endif
